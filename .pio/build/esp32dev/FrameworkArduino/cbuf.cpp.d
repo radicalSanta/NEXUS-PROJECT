57d328bc55a22b01d684eac715c45a43edc7886a
radicalSanta <samridhsarkar.2005@gmail.com>
2026-09-06 15:04:27 +0530

    Organize firmware into NEXUS-FIRMWARE directory

---
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ /home/celric/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /home/celric/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/esp32dev/src/CircularBuffer.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/src/CircularBuffer.cpp.o: src/CircularBuffer.cpp \
+ src/CircularBuffer.h
